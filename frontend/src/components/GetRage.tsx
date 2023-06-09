082d918749c3c54fdd770c5410cacf98386d2376
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-06-10 11:04:11 +1200

    Added a table to view submitted rages

--- frontend/src/components/GetRage.tsx
+++ frontend/src/components/GetRage.tsx
@@ -1,20 +1,44 @@
-import { DATABASE_URL } from '../helpers/helpers'
+import { DATABASE_URL, Complaint } from '../helpers/helpers'
 
 import { useQuery } from '@tanstack/react-query'
 
+import { createColumnHelper } from '@tanstack/react-table'
+import Table from './table/Table'
+
+const columnHelper = createColumnHelper<Complaint>()
+
+const columns = [
+    columnHelper.accessor('submissionTime', {
+        header: 'Time',
+        cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor('complaint', {
+        header: 'Complaint',
+        cell: (info) => info.getValue(),
+    }),
+]
+
 export default function GetRage() {
-    const { isLoading, error, data } = useQuery(['testData'], () =>
+    const { data } = useQuery(['complaints'], () =>
         fetch(DATABASE_URL + 'key/complaints', {
             method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
+                Accept: 'application/json',
                 Authorization: 'Basic ' + btoa('root:root'),
                 NS: 'test',
                 DB: 'test',
             },
-        })
+        }).then((res) => res.json())
     )
-    console.log(data)
 
-    return <div>Loaded data</div>
+    return (
+        <div>
+            Rages Sent:
+            {data && data[0].result?.length > 0 ? (
+                <Table columns={columns} data={data[0].result} />
+            ) : (
+                <p>No Rage</p>
+            )}
+        </div>
+    )
 }
--- frontend/src/helpers/helpers.tsx
+++ frontend/src/helpers/helpers.tsx
@@ -1,1 +1,6 @@
 export const DATABASE_URL = 'http://localhost:9000/'
+
+export type Complaint = {
+    complaint: string
+    submissionTime: Date
+}
