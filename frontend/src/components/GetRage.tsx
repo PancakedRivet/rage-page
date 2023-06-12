9bb9a04653cacfe1ca6ee977dfea397643ec945b
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-06-12 20:59:30 +1200

    Adding tags as a seperate table

--- frontend/src/components/GetRage.tsx
+++ frontend/src/components/GetRage.tsx
@@ -8,10 +8,15 @@ import { createColumnHelper } from '@tanstack/react-table'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
+import NivoLine from './graph/NivoLine'
 
 const columnHelper = createColumnHelper<Complaint>()
 
 const columns = [
+    columnHelper.accessor('id', {
+        header: 'Id',
+        cell: (info) => info.getValue(),
+    }),
     columnHelper.accessor('submissionTime', {
         header: 'Time',
         cell: (info) => info.getValue(),
@@ -19,6 +24,16 @@ const columns = [
     columnHelper.accessor('complaint', {
         header: 'Complaint',
         cell: (info) => info.getValue(),
+    }),
+    columnHelper.accessor('tags', {
+        header: 'Tag',
+        cell: (info) => {
+            const cellValue = info.getValue()
+            if (Array.isArray(cellValue)) {
+                return cellValue.join(', ')
+            }
+            return info.getValue()
+        },
     }),
 ]
 
@@ -35,6 +50,20 @@ export default function GetRage() {
         }).then((res) => res.json())
     )
 
+    const { data: tagData } = useQuery(['tags'], () =>
+        fetch(DATABASE_URL + 'key/tags', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Authorization: 'Basic ' + btoa('root:root'),
+                NS: 'test',
+                DB: 'test',
+            },
+        }).then((res) => res.json())
+    )
+
+    console.log(tagData)
+
     return (
         <Box sx={{ width: '100%' }}>
             <Stack spacing={2}>
@@ -42,11 +71,14 @@ export default function GetRage() {
                     Rages Sent:
                 </Typography>
                 {data && data[0].result?.length > 0 ? (
-                    <ReactTable
-                        columns={columns}
-                        data={data[0].result}
-                        // showTableState
-                    />
+                    <>
+                        <ReactTable
+                            columns={columns}
+                            data={data[0].result}
+                            // showTableState
+                        />
+                        <NivoLine data={data[0].result} />
+                    </>
                 ) : (
                     <Typography variant="h4" gutterBottom>
                         No Rages Sent!
--- frontend/src/helpers/helpers.tsx
+++ frontend/src/helpers/helpers.tsx
@@ -1,6 +1,8 @@
 export const DATABASE_URL = 'http://localhost:9000/'
 
 export type Complaint = {
+    id: string
     complaint: string
     submissionTime: Date
+    tags: string[]
 }
