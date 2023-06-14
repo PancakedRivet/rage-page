fc5641f45835553533439d6ca1b9a4796228ec1e
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-06-14 13:50:38 +1200

    Updating to time series example

--- frontend/src/components/graph/NivoLine.tsx
+++ frontend/src/components/graph/NivoLine.tsx
@@ -284,26 +284,46 @@ const NivoLine = ({ data }: any) => (
     <div className="nivo">
         <ResponsiveLine
             // data={data}
-            data={data ? data : exampleData}
+            // data={data ? data : exampleData}
+            data={[
+                {
+                    id: 'fake corp. A',
+                    data: [
+                        { x: '2018-01-01', y: 7 },
+                        { x: '2018-01-02', y: 5 },
+                        { x: '2018-01-03', y: 11 },
+                        { x: '2018-01-04', y: 9 },
+                        { x: '2018-01-05', y: 12 },
+                        { x: '2018-01-06', y: 16 },
+                        { x: '2018-01-07', y: 13 },
+                        { x: '2018-01-08', y: 13 },
+                    ],
+                },
+                {
+                    id: 'fake corp. B',
+                    data: [
+                        { x: '2018-01-04', y: 14 },
+                        { x: '2018-01-05', y: 14 },
+                        { x: '2018-01-06', y: 15 },
+                        { x: '2018-01-07', y: 11 },
+                        { x: '2018-01-08', y: 10 },
+                        { x: '2018-01-09', y: 12 },
+                        { x: '2018-01-10', y: 9 },
+                        { x: '2018-01-11', y: 7 },
+                    ],
+                },
+            ]}
             margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-            xScale={{ type: 'point' }}
+            xScale={{
+                type: 'time',
+                format: '%Y-%m-%d',
+                useUTC: false,
+                precision: 'day',
+            }}
+            xFormat="time:%Y-%m-%d"
             yScale={{
                 type: 'linear',
-                min: 'auto',
-                max: 'auto',
-                stacked: true,
-                reverse: false,
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'transportation',
-                legendOffset: 36,
-                legendPosition: 'middle',
+                // stacked: boolean('stacked', false),
             }}
             axisLeft={{
                 tickSize: 5,
@@ -313,12 +333,20 @@ const NivoLine = ({ data }: any) => (
                 legendOffset: -40,
                 legendPosition: 'middle',
             }}
+            axisBottom={{
+                format: '%b %d',
+                tickValues: 'every 2 days',
+                legend: 'time scale',
+                legendOffset: -12,
+            }}
+            enablePointLabel={true}
             pointSize={10}
             pointColor={{ theme: 'background' }}
             pointBorderWidth={2}
             pointBorderColor={{ from: 'serieColor' }}
             pointLabelYOffset={-12}
             useMesh={true}
+            enableSlices={false}
             legends={[
                 {
                     anchor: 'bottom-right',
@@ -347,6 +375,24 @@ const NivoLine = ({ data }: any) => (
             ]}
         />
     </div>
+)
+
+const CustomSymbol = ({ size, color, borderWidth, borderColor }) => (
+    <g>
+        <circle
+            fill="#fff"
+            r={size / 2}
+            strokeWidth={borderWidth}
+            stroke={borderColor}
+        />
+        <circle
+            r={size / 5}
+            strokeWidth={borderWidth}
+            stroke={borderColor}
+            fill={color}
+            fillOpacity={0.35}
+        />
+    </g>
 )
 
 export default NivoLine
