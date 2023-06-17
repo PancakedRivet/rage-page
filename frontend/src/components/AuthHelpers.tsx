c89b27212286a2b6d13735d48ddab9000bb70b04
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-06-17 13:13:36 +1200

    Updated the login page

--- frontend/src/components/AuthHelpers.tsx
+++ frontend/src/components/AuthHelpers.tsx
@@ -1,21 +1,46 @@
-import React from 'react'
-import { useNavigate, useLocation, Navigate } from 'react-router-dom'
+import React, { useRef } from 'react'
+
 import { useAuth } from './contexts/AuthContext'
+
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import FormControl from '@mui/material/FormControl'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import InputLabel from '@mui/material/InputLabel'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import Typography from '@mui/material/Typography'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+
+import LoginIcon from '@mui/icons-material/Login'
+
+import { useNavigate, useLocation, Navigate } from 'react-router-dom'
 
 export function LoginPage() {
     const navigate = useNavigate()
     const location = useLocation()
     const auth = useAuth()
 
+    const valueRef = useRef('')
+
     const from = location.state?.from?.pathname || '/'
 
-    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    const [isPasswordError, setIsPasswordError] = React.useState<boolean>(false)
+
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show)
+
+    const handleMouseDownPassword = (
+        event: React.MouseEvent<HTMLButtonElement>
+    ) => {
         event.preventDefault()
+    }
 
-        const formData = new FormData(event.currentTarget)
-        const password = formData.get('password') as string
-
-        if (password === import.meta.env.VITE_ADMIN_PASSWORD) {
+    const handleSubmitLogin = () => {
+        const submittedPassword = valueRef.current.value
+        if (submittedPassword === import.meta.env.VITE_ADMIN_PASSWORD) {
             auth.signin(() => {
                 // Send them back to the page they tried to visit when they were
                 // redirected to the login page. Use { replace: true } so we don't create
@@ -25,25 +50,59 @@ export function LoginPage() {
                 // user experience.
                 navigate(from, { replace: true })
             })
+        } else {
+            setIsPasswordError(true)
         }
     }
 
     return (
-        <div>
-            <p>You must log in to view the page at {from}</p>
-
-            <pre>
-                import.meta.env.VITE_ADMIN_PASSWORD:{' '}
-                {import.meta.env.VITE_ADMIN_PASSWORD}
-            </pre>
-
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Password: <input name="password" type="password" />
-                </label>{' '}
-                <button type="submit">Login</button>
-            </form>
-        </div>
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <Typography variant="h4" gutterBottom>
+                You must log in to view the page at {from}
+            </Typography>
+            <FormControl variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                </InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {showPassword ? (
+                                    <VisibilityOff />
+                                ) : (
+                                    <Visibility />
+                                )}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    label="Password"
+                    error={isPasswordError}
+                    inputRef={valueRef}
+                />
+                <Button
+                    onClick={handleSubmitLogin}
+                    variant="contained"
+                    endIcon={<LoginIcon />}
+                >
+                    Login
+                </Button>
+            </FormControl>
+        </Box>
     )
 }
 
