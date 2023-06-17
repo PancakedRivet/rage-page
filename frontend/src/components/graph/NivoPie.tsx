f5fcf762147ba3c9b73efb7941c543c595e74dbe
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-06-18 10:47:49 +1200

    Pie chart working

--- frontend/src/components/graph/NivoPie.tsx
+++ frontend/src/components/graph/NivoPie.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useOrdinalColorScale } from '@nivo/colors'
 import { ResponsivePie } from '@nivo/pie'
+import { useTheme } from '@mui/material/styles'
+import { DARK, LIGHT } from './theme'
 
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
 export const NivoPie = ({ data }: any) => {
+    const theme = useTheme()
+    const isDarkMode = theme.palette.mode === 'light' ? false : true
+    const colors = useOrdinalColorScale({ scheme: 'category10' }, 'id')
+
     return (
         <div className="nivo">
             <ResponsivePie
                 data={data}
                 margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                theme={isDarkMode ? DARK : LIGHT}
+                colors={(d) => colors(d)}
                 innerRadius={0.5}
                 padAngle={0.7}
                 cornerRadius={3}
@@ -22,7 +26,6 @@ export const NivoPie = ({ data }: any) => {
                     modifiers: [['darker', 0.2]],
                 }}
                 arcLinkLabelsSkipAngle={10}
-                arcLinkLabelsTextColor="#333333"
                 arcLinkLabelsThickness={2}
                 arcLinkLabelsColor={{ from: 'color' }}
                 arcLabelsSkipAngle={10}
@@ -30,76 +33,6 @@ export const NivoPie = ({ data }: any) => {
                     from: 'color',
                     modifiers: [['darker', 2]],
                 }}
-                defs={[
-                    {
-                        id: 'dots',
-                        type: 'patternDots',
-                        background: 'inherit',
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        size: 4,
-                        padding: 1,
-                        stagger: true,
-                    },
-                    {
-                        id: 'lines',
-                        type: 'patternLines',
-                        background: 'inherit',
-                        color: 'rgba(255, 255, 255, 0.3)',
-                        rotation: -45,
-                        lineWidth: 6,
-                        spacing: 10,
-                    },
-                ]}
-                fill={[
-                    {
-                        match: {
-                            id: 'ruby',
-                        },
-                        id: 'dots',
-                    },
-                    {
-                        match: {
-                            id: 'c',
-                        },
-                        id: 'dots',
-                    },
-                    {
-                        match: {
-                            id: 'go',
-                        },
-                        id: 'dots',
-                    },
-                    {
-                        match: {
-                            id: 'python',
-                        },
-                        id: 'dots',
-                    },
-                    {
-                        match: {
-                            id: 'scala',
-                        },
-                        id: 'lines',
-                    },
-                    {
-                        match: {
-                            id: 'lisp',
-                        },
-                        id: 'lines',
-                    },
-                    {
-                        match: {
-                            id: 'elixir',
-                        },
-                        id: 'lines',
-                    },
-                    {
-                        match: {
-                            id: 'javascript',
-                        },
-                        id: 'lines',
-                    },
-                ]}
                 legends={[
                     {
                         anchor: 'bottom',
@@ -110,19 +43,10 @@ export const NivoPie = ({ data }: any) => {
                         itemsSpacing: 0,
                         itemWidth: 100,
                         itemHeight: 18,
-                        itemTextColor: '#999',
                         itemDirection: 'left-to-right',
                         itemOpacity: 1,
                         symbolSize: 18,
                         symbolShape: 'circle',
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemTextColor: '#000',
-                                },
-                            },
-                        ],
                     },
                 ]}
             />
